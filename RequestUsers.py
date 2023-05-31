c6bfac9ed9fedd0d479555223cb06d32ca1b53a2
Wenz Tecnologia <38402865+ViniciusMozart@users.noreply.github.com>
2023-05-30 21:40:10 -0300

    Update RequestUsers.py

--- RequestUsers.py
+++ RequestUsers.py
@@ -1,43 +1,56 @@
-# Importar as bibliotecas necessárias
-import requests
-import json
-import os
+import time
 
-# Definir as variáveis de configuração
-#tenant_id = "seu_id_do_tenant"
-tenant_id = os.getenv("TENANT_ID")
+while True:
+    # Coloque o seu código aqui
+    # Importar as bibliotecas necessárias
+    import requests
+    import json
+    import os
+    import logging
 
-#client_id = "seu_id_do_client"
-client_id = os.getenv("CLIENT_ID")
+    # Definir as variáveis de configuração
+    #tenant_id = "seu_id_do_tenant"
+    tenant_id = os.getenv("TENANT_ID")
 
-#client_secret = "seu_secret_do_client"
-client_secret = os.getenv("CLIENT_SECRET")
+    #client_id = "seu_id_do_client"
+    client_id = os.getenv("CLIENT_ID")
 
-scope = "https://graph.microsoft.com/.default"
+    #client_secret = "seu_secret_do_client"
+    client_secret = os.getenv("CLIENT_SECRET")
 
-#group_id = "seu_id_do_grupo_dinamico"
-group_id = os.getenv("GROUP_ID")
+    scope = "https://graph.microsoft.com/.default"
 
-# Obter o token de acesso do Azure AD usando o service principal
-url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
-data = {
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "scope": scope,
-    "grant_type": "client_credentials"
-}
-response = requests.post(url, data=data)
-token = response.json().get("access_token")
-#print(token)
+    #group_id = "seu_id_do_grupo_dinamico"
+    group_id = os.getenv("GROUP_ID")
 
-# Usar o token de acesso para consultar os usuários do grupo dinâmico no Microsoft Graph
-headers = {
-    "Authorization": f"Bearer {token}"
-}
-url = f"https://graph.microsoft.com/v1.0/groups/{group_id}/members"
-response = requests.get(url, headers=headers)
-users = response.json().get("value")
+    # Obter o token de acesso do Azure AD usando o service principal
+    url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+    data = {
+     "client_id": client_id,
+     "client_secret": client_secret,
+     "scope": scope,
+     "grant_type": "client_credentials"
+    }
+    response = requests.post(url, data=data)
+    token = response.json().get("access_token")
+    print(token)
 
-# Salvar os usuários em um arquivo user.json na raiz do script
-with open("data/user.json", "w") as file:
-    json.dump(users, file, indent=4)
+    # Usar o token de acesso para consultar os usuários do grupo dinâmico no Microsoft Graph
+    headers = {
+     "Authorization": f"Bearer {token}"
+    }
+    url = f"https://graph.microsoft.com/v1.0/groups/{group_id}/members"
+    try:
+        response = requests.get(url, headers=headers)
+        users = response.json().get("value")
+    except json.JSONDecodeError as e:
+        logging.error(f'Erro ao decodificar JSON: {e}')
+        users = None
+
+    # Salvar os usuários em um arquivo user.json na raiz do script
+    if users:
+        with open("data/user.json", "w") as file:
+            json.dump(users, file, indent=4)
+
+    # Pausar a execução por 2 minutos
+    time.sleep(15)
